2e58c30fcfa29842bd1dd99524664c5ee03d7511
MikhailZavialov <mishazav98@gmail.com>
2019-08-30 11:55:15 +0300

    change2

---
+++ src/services/swapi-service.js
@@ -0,0 +1,131 @@
+export  default class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url){
+        const res = await fetch(`${this._apiBase}${url}`); 
+
+        if (!res.ok){
+            throw new Error (`Could not fetch ${url}` +
+                `, received ${res.status}`)
+        }
+        return  await res.json();
+    }
+    async getAllPeople(){
+        const res = await this.getResource(`/people/`);
+        return res.results.map(this._transformPPerson)
+    }
+
+    getPerson(id) {
+        const person =  this.getResource(`/people/${id}/`);
+        return this._transformPPerson(person);
+    }
+
+    async getAllPlanets(){
+        const res = await this.getResource(`/planets/`);
+        return res.results.map(this._transformPlanet);
+    }
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet);
+    }
+
+    async getAllStarships(){
+        const res = await this.getResource(`/starships/`);
+        return res.results.map(this._transformStarship);
+    }
+    getStarship(id) {
+        const starship =  this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starship);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet){
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
+    }
+
+    _transformStarship(starship) {
+        return {
+            id : this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            lenght: starship.lenght,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+    _transformPPerson(person){
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
+}
+
+
+
+
+
+
+// const getResource = async (url) => {
+//     const res = await fetch(url);
+//
+//     if (!res.ok){
+//         throw new Error (`Could not fetch ${url}` +
+//         `, received ${res.status}`)
+//     }
+//     return  await res.json();
+//
+// };
+// getResource('https://swapi.co/api/people/1/')
+//     .then((body) => {
+//         console.log(body);
+//     })
+//
+//     .catch((err) => {
+//         console.error('Could not fetch', err);
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
